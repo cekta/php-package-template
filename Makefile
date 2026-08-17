86f8f11b7440fa6b279b83db34f6e4dcc3ce28ea
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-08-18 00:32:59 +0500

    test-8.3 to php-8.3 ...

--- Makefile
+++ Makefile
@@ -8,31 +8,31 @@ shell: dev
 
 .PHONY: ci
 ci:
-	docker compose run --rm app composer test
+	docker compose run --rm app composer ci
 
-.PHONY: test-8.3
-test-8.3:
+.PHONY: php-8.3
+php-8.3:
 	docker compose down
 	PHP_VERSION=8.3 docker compose build
 	$(MAKE) dev
 	docker compose exec app composer update
-	docker compose exec app composer test
+	docker compose exec app composer ci
 
-.PHONY: test-8.4
-test-8.4:
+.PHONY: php-8.4
+php-8.4:
 	docker compose down
 	PHP_VERSION=8.4 docker compose build
 	$(MAKE) dev
 	docker compose exec app composer update
-	docker compose exec app composer test
+	docker compose exec app composer ci
 
-.PHONY: test-8.5
-test-8.5:
+.PHONY: php-8.5
+php-8.5:
 	docker compose down
 	PHP_VERSION=8.5 docker compose build
 	$(MAKE) dev
 	docker compose exec app composer update
-	docker compose exec app composer test
+	docker compose exec app composer ci
 
 .PHONY: docs-shell
 docs-shell: dev
--- testo.php
+++ testo.php
@@ -4,20 +4,20 @@ declare(strict_types=1);
 
 use Testo\Application\Config\ApplicationConfig;
 use Testo\Application\Config\SuiteConfig;
-use Testo\Codecov\CodecovPlugin;
-use Testo\Codecov\Report\PhpUnitXmlReport;
+use Testo\Convention\NamingConventionPlugin;
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    // Преобразуем предупреждения (warnings), уведомления (notices) и ошибки пользователя (user-errors) в исключения
+    if (in_array($errno, [E_WARNING, E_NOTICE, E_USER_WARNING, E_USER_NOTICE, E_DEPRECATED, E_USER_DEPRECATED], true)) {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+    return false;
+});
 
 return new ApplicationConfig(
     src: ["src"],
-    plugins: [
-        new CodecovPlugin(
-            reports: [
-                // required fpr infection
-                new PhpUnitXmlReport(
-                    "/tmp/infection/infection/coverage-xml",
-                ),
-            ],
-        ),
-    ],
     suites: [new SuiteConfig(name: "Unit", location: ["tests"])],
+    plugins: [
+        new NamingConventionPlugin(),
+    ],
 );
